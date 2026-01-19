58a8af9939dcc1d71aaa40b7cbcac12dab1c37ac
rahulsnair <rahulsnair.vdp@gmail.com>
2026-01-19 21:00:36 +0530

    add nsf to NFC SKU

--- device.mk
+++ device.mk
@@ -242,7 +242,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
 
-DEVICE_NFC_SKUS := dns dnsf ns
+DEVICE_NFC_SKUS := dns dnsf ns nsf
 
 PRODUCT_COPY_FILES += \
     $(foreach DEVICE_NFC_SKU, $(DEVICE_NFC_SKUS), \
