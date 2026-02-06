e44312b68d6b2440483880e5513371cb1e6b4e79
rahulsnair <rahulsnair.vdp@gmail.com>
2026-02-06 18:56:29 +0530

    switch from prebuilt kernel, dtb and modules to source built

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,17 +94,19 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=11201000.usb0
 
 TARGET_FORCE_PREBUILT_KERNEL := true
-BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)-kernel/dtb
-PRODUCT_COPY_FILES += \
-	$(DEVICE_PATH)-kernel/Image.gz:kernel
 
-BOARD_SYSTEM_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)-kernel/system/*.ko)
-BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)-kernel/modules.load.system))
-BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)-kernel/vendor/*.ko)
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)-kernel/modules.load.vendor))
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)-kernel/vendor_ramdisk/*.ko)
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)-kernel/modules.load.vendor_ramdisk))
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)-kernel/modules.load.recovery))
+KERNEL_BAZEL_BUILD_OUT := out/target/product/${TARGET_BOOTLOADER_BOARD_NAME}/obj/KLEAF_OBJ/dist
+BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_BAZEL_BUILD_OUT)/dtbs
+PRODUCT_COPY_FILES += \
+	$(KERNEL_BAZEL_BUILD_OUT)/Image.gz:kernel
+
+BOARD_SYSTEM_KERNEL_MODULES := $(wildcard $(KERNEL_BAZEL_BUILD_OUT)/system/*.ko)
+BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.system))
+BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(KERNEL_BAZEL_BUILD_OUT)/vendor/*.ko)
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(wildcard $(KERNEL_BAZEL_BUILD_OUT)/vendor_ramdisk/*.ko)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_ramdisk))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
 BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
 
 # Recovery touch modules (from vendor_dlkm)
@@ -115,7 +117,7 @@ RECOVERY_KERNEL_MODULES := \
     touchscreen_u_mmi \
     focaltech_touch_v3_u_mmi
 
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(addprefix $(DEVICE_PATH)-kernel/vendor/,$(addsuffix .ko,$(RECOVERY_KERNEL_MODULES)))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_BAZEL_BUILD_OUT)/vendor/,$(addsuffix .ko,$(RECOVERY_KERNEL_MODULES)))
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(RECOVERY_KERNEL_MODULES)
 
 BOARD_MKBOOTIMG_ARGS += \
