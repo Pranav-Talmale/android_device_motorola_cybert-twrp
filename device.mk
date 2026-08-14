23839092599c6f1eea4b9d8800888d3ae12d6e9d
Pranav-Talmale <pranavtalmale@gmail.com>
2026-08-14 13:34:28 +0530

    Build mtk bootctrl and mtk_plpath

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.threads=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
     FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE) \
     POSTINSTALL_OPTIONAL_system=true
 
@@ -53,10 +53,18 @@ PRODUCT_PACKAGES += \
     update_engine_sideload \
     update_verifier
 
-# Bootcontrol
+# Boot control HAL
 PRODUCT_PACKAGES += \
-    com.android.hardware.boot \
-    android.hardware.boot-service.default_recovery
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+# MTK plpath utils
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils \
+    mtk_plpath_utils.recovery
 
 # Fastboot
 PRODUCT_PACKAGES += \
